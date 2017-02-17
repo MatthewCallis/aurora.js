dbd658a24131c01c53686464a9b5e0140ee23f6f
Matthew Callis <matthew.callis@gmail.com>
2017-02-17 10:02:27 -0800

    Clean up error file logic

--- src/core/underflow_error.js
+++ src/core/underflow_error.js
@@ -3,10 +3,11 @@ export default class AVUnderflowError extends Error {
     super(message);
 
     this.name = 'AVUnderflowError';
+    this.stack = (new Error(message)).stack;
+    /* istanbul ignore else */
+    // https://nodejs.org/api/errors.html#errors_error_capturestacktrace_targetobject_constructoropt
     if (typeof Error.captureStackTrace === 'function') {
       Error.captureStackTrace(this, this.constructor);
-    } else {
-      this.stack = (new Error(message)).stack;
     }
   }
 }
